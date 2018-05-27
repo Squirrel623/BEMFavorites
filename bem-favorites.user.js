51e9e8b6e94b7f9715a7ce446580c619f51c3bb5
Brady <squirrel623@yahoo.com>
2018-05-27 18:51:39 -0400

    fix(userscript): 'Bem' needs to be defined before our file is loaded

--- bem-favorites.user.js
+++ bem-favorites.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       BEMFavorites
-// @version    0.3
+// @version    0.4
 // @author     Squirrel623
 // @description  Add a favorites section to Berryemotes
 // @match      http://www.berrytube.tv/*
@@ -8,7 +8,25 @@
 // @updateURL  https://squirrel623.github.io/BEMFavorites/bem-favorites.user.js
 // ==/UserScript==
 
-var script=document.createElement('script');
-script.setAttribute("type","text/javascript");
-script.setAttribute("src", "https://squirrel623.github.io/BEMFavorites/dist/bem-favorites.min.js");
-document.head.appendChild(script);
+// The script we are trying to load uses webpack that shims out Bem. It needs the global
+// Bem object to be created before the script it actually loaded
+var bemfMaxIterations = 200;
+var bemfIteration = 0;
+
+var bemfIntervalId = setInterval(function() {
+  bemfIteration++;
+  if (bemfIteration > bemfMaxIterations) {
+    clearInterval(bemfIntervalId);
+    console.warn('Berry Emotes not enabled or did not load fast enough. Giving up.');
+  }
+
+  if (Bem === undefined) {
+    return;
+  }
+
+  clearInterval(bemfIntervalId);
+  var bemfScript=document.createElement('script');
+  bemfScript.setAttribute("type","text/javascript");
+  bemfScript.setAttribute("src", "https://squirrel623.github.io/BEMFavorites/dist/bem-favorites.min.js");
+  document.head.appendChild(bemfScript);
+}, 100);
