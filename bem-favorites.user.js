55ad1c8d658715be5953f2cd6c02549c5027f55b
Brady <squirrel623@yahoo.com>
2018-05-27 18:32:31 -0400

    fix(userscript): Fix links, we don't need github pages lol

--- bem-favorites.user.js
+++ bem-favorites.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name       BEMFavorites
 // @version    0.1
+// @author     Squirrel623
 // @description  Add a favorites section to Berryemotes
 // @match      http://www.berrytube.tv/*
 // @match      http://berrytube.tv/*
-// @updateURL  https://squirrel623.github.io/BEMFavorites/userscript.js
+// @updateURL  https://github.com/Squirrel623/BEMFavorites/raw/master/bem-favorites.user.js
 // ==/UserScript==
 
 var script=document.createElement('script');
 script.setAttribute("type","text/javascript");
-script.setAttribute("src", "https://squirrel623.github.io/BEMFavorites/dist/bem-favorites.min.js");
+script.setAttribute("src", "https://github.com/Squirrel623/BEMFavorites/raw/master/dist/bem-favorites.min.js");
 document.head.appendChild(script);
