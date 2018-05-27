0584625878a6384c30b47fe2e3553b9e708da3e3
Brady <squirrel623@yahoo.com>
2018-05-27 18:39:55 -0400

    We actually do need it so it is served as a js file and not plaintext

--- bem-favorites.user.js
+++ bem-favorites.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name       BEMFavorites
-// @version    0.2
+// @version    0.3
 // @author     Squirrel623
 // @description  Add a favorites section to Berryemotes
 // @match      http://www.berrytube.tv/*
 // @match      http://berrytube.tv/*
-// @updateURL  https://github.com/Squirrel623/BEMFavorites/raw/master/bem-favorites.user.js
+// @updateURL  https://squirrel623.github.io/BEMFavorites/bem-favorites.user.js
 // ==/UserScript==
 
 var script=document.createElement('script');
 script.setAttribute("type","text/javascript");
-script.setAttribute("src", "https://github.com/Squirrel623/BEMFavorites/raw/master/dist/bem-favorites.min.js");
+script.setAttribute("src", "https://squirrel623.github.io/BEMFavorites/dist/bem-favorites.min.js");
 document.head.appendChild(script);
